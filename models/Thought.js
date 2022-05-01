471bf0900dca0d75bbdb7bb01db09366fcb25c9d
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-05-01 16:32:53 -0500

    adds Thought model and reaction schema

---
+++ models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, model, Types } = require('mongoose')
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionsSchema]
+},
+{
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+});
+
+const ReactionsSchema = new Schema ({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    }, 
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+})
+
+ReactionsSchema.virtual('reactionCount'). get(function() {
+    return this.reactions.length;
+})
+
+const Thought = model('Thought', ThoughtSchema);
+  
+module.exports = Thought;
