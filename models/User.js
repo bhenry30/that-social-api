43d84bf72ed59a14571c149e60cdf21c187d7def
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-05-01 15:51:01 -0500

    adds User model schema

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+        }
+    ],
+},       
+{
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+      id: false
+});
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
